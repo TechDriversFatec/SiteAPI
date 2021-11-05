a58a0ad02d204ed1326b2222e44ca2ba3796041e
Larissa Diniz <diniz.laari@gmail.com>
2021-11-05 10:30:09 -0300

    Update main.js
    Botão voltar ao topo

--- Javascript/main.js
+++ Javascript/main.js
@@ -56,3 +56,18 @@ $(function () {
     }
   })
 })
+
+/*botão voltar para o topo*/
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*quando rolar a página*/
+window.addEventListener('scroll', function () {
+  backToTop()
+})
